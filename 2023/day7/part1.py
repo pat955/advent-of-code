9c38ef3ec687a6b10250c94117934b84cf2cbef2
pat955 <patricijap8@gmail.com>
2024-03-04 11:21:23 +0100

    committing before making big changes

--- 2023/day7/part1.py
+++ 2023/day7/part1.py
@@ -1,13 +1,55 @@
 import re
-
-with open('test.txt', 'r') as file:
-    rank_list = []
-    for line in file:
-        hand, bet = line.split()
+def main():
+    
+    with open('input.txt', 'r') as file:
+        rank_dict = {}
+        for line in file:
+            hand, bet = line.split()
+            get_type(hand)
+            
         
 
 def get_type(hand):
-    first_card = hand[0]
-    if hand == first_card*5:
-        return ((4, first_card))
+    hand_hierarchy = {
+        'Five of a kind',
+        'Four of a kind',
+        'Full house',
+        'Three of a kind',
+        'Two pair',
+        'One pair',
+        'High card'
+    }
+    values =['A','2','3','4','5','6','7','8','9','T','J','Q','K']
+
+    hand_sorted = sorted([*hand], key=values.index)
+    print(hand_sorted)
+    hand_set = set([*hand])
+    hand_length = len(hand_set)
+
+    if hand_length == 0:
+        return (hand[0], 'Five of a kind')
     
+    if hand_length == 2:
+        first_card = hand[0]
+        first_card_count = [*hand].count(first_card)
+        if first_card_count == 4:
+            return (first_card, 'Four of a kind')
+
+        elif first_card_count == 1:
+            return (hand[1], 'Four of a kind')
+        else:
+            return ('Fullhouse')
+
+    if hand_length == 3:
+        pass
+
+    
+
+def partition(rank_dict):
+    pass
+
+def quicksort(rank_dict):
+    pass
+
+
+main()
